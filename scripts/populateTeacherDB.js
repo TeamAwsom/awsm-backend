019a3a628811144767c30ea8d3debfce9738209a
esack7 <isaacheist@gmail.com>
2018-09-18 18:01:05 -0700

    added more example teachers

--- scripts/populateTeacherDB.js
+++ scripts/populateTeacherDB.js
@@ -82,6 +82,163 @@ const sampleTeachers = [
     ],
     students: [],
   },
+  {
+    name: 'Peter Billings',
+    email: 'petebills@gmail.com',
+    address: '19100 44th Ave W, Lynnwood, WA 98046',
+    availability: [
+      {
+        day: 'monday',
+        fromTime: 17,
+        toTime: 21,
+      },
+      {
+        day: 'tuesday',
+        fromTime: 17,
+        toTime: 21,
+      },
+      {
+        day: 'wednesday',
+        fromTime: 17,
+        toTime: 21,
+      },
+      {
+        day: 'thursday',
+        fromTime: 17,
+        toTime: 21,
+      },
+      {
+        day: 'friday',
+        fromTime: 17,
+        toTime: 21,
+      },
+      {
+        day: 'saturday',
+        fromTime: 8,
+        toTime: 17,
+      },
+    ],
+    students: [],
+  },
+  {
+    name: 'Susan Kentworth',
+    email: 'suzie1986@gmail.com',
+    address: '220 Fourth Ave. S., Kent, WA 98032',
+    availability: [
+      {
+        day: 'monday',
+        fromTime: 13,
+        toTime: 20,
+      },
+      {
+        day: 'tuesday',
+        fromTime: 15,
+        toTime: 21,
+      },
+      {
+        day: 'wednesday',
+        fromTime: 17,
+        toTime: 19,
+      },
+      {
+        day: 'thursday',
+        fromTime: 13,
+        toTime: 20,
+      },
+      {
+        day: 'sunday',
+        fromTime: 8,
+        toTime: 17,
+      },
+    ],
+    students: [],
+  },
+  {
+    name: 'Alma Luna-Mendez',
+    email: 'soulmoon@gmail.com',
+    address: '9600 College Way N, Seattle, WA 98103',
+    availability: [
+      {
+        day: 'monday',
+        fromTime: 6,
+        toTime: 12,
+      },
+      {
+        day: 'tuesday',
+        fromTime: 13,
+        toTime: 17,
+      },
+      {
+        day: 'wednesday',
+        fromTime: 6,
+        toTime: 15,
+      },
+      {
+        day: 'thursday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'friday',
+        fromTime: 6,
+        toTime: 12,
+      },
+      {
+        day: 'saturday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'sunday',
+        fromTime: 13,
+        toTime: 21,
+      },
+    ],
+    students: [],
+  },
+  {
+    name: 'Alma Luna-Mendez',
+    email: 'soulmoon@gmail.com',
+    address: '9600 College Way N, Seattle, WA 98103',
+    availability: [
+      {
+        day: 'monday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'tuesday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'wednesday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'thursday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'friday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'saturday',
+        fromTime: 8,
+        toTime: 17,
+      },
+      {
+        day: 'sunday',
+        fromTime: 8,
+        toTime: 17,
+      },
+    ],
+    students: [],
+  },
 ];
 
 Teacher.insertMany(sampleTeachers)
