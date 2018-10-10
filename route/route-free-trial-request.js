621e35e7ce155cb0d17fc805848099c778fae2e3
Eric Pak <pak.eric@gmail.com>
2018-10-09 19:01:21 -0700

    commented

--- route/route-free-trial-request.js
+++ route/route-free-trial-request.js
@@ -48,6 +48,7 @@ module.exports = router => {
       const studentAvailability = makeAvailability(availability);
 
       req.body.instrument = req.body.instrument.toUpperCase();
+      console.log(req.body.instrument);
       const student = await new Student(req.body).save();
 
       postResponse.studentID = student._id;
@@ -58,7 +59,7 @@ module.exports = router => {
       );
 
       if (!teachers.length) {
-        return res.status(200).send({ message: 'no matches found' });
+        return res.status(200).send({ message: 'no matches found: Instrument' });
       }
 
       const availableTeachers = filterTeachersByAvailability(
@@ -67,7 +68,7 @@ module.exports = router => {
       );
 
       if (!availableTeachers.length) {
-        return res.status(200).send({ message: 'no matches found' });
+        return res.status(200).send({ message: 'no matches found: Availability' });
       }
 
       const studentAddress = createAddress(req.body);
